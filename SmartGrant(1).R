0f6f46fd4f6ce39662ead3f24f4c8651e369dd76
Skye <163369285+ESkyeD@users.noreply.github.com>
2024-07-25 15:14:55 -0400

    Update SmartGrant(1).R
    Fixes as.data.frame and convert, to use Tidy instead

--- SmartGrant(1).R
+++ SmartGrant(1).R
@@ -1,143 +1,103 @@
-library(stringr)
-library(ggplot2)
-library(cowplot)
-library(quanteda)
-library(quanteda.corpora)
-library(quanteda.textplots)
-library(quanteda.textstats)
-library(stopwords)
-library (dplyr)
-library(rio)
-library(ggplot2)
-library(DescTools)
-library(tidyr)
-library(tibble)
-library(haven)
-library(gridExtra)
-library(reshape2)
-###### 2. Reading in documents and creating a corpus. ###### 
+libraries <- c("stringr", "ggplot2", "cowplot", "quanteda", "quanteda.corpora", 
+               "quanteda.textplots", "quanteda.textstats", "stopwords", "dplyr", 
+               "rio", "DescTools", "tidyr", "tibble", "haven", "gridExtra", 
+               "reshape2", "tidytext", "wordcloud", "RColorBrewer")
 
-# There are a few of different standard ways that you may save your data (i.e., documents) in:
-# 1. as a data frame, where the text is in a column and associated information or metadata is in the neighboring columns;
-# 2. as a collection of separate text documents that are housed in the same folder. 
-# First, let's grab the list of ile names that we need to read in.
-grant.list = list.files("./SMART GRANT")
-grant.list = paste("./SMART GRANT/",grant.list,sep="")
-# Next let's read in the documents. 
-documents = data.frame("filepath" = rep(NA,length(grant.list)),
-                       "name" = rep(NA,length(grant.list)),
-                       "grant" = rep(NA,length(grant.list)))
-
-for(i in 1:length(grant.list)){
-  
-  cat("currently working on bill:",i,"\n")
-  
-  # log the file path 
-  documents$filepath[i] = grant.list[i]
-  #add a file name
-  documents$name[i]<-documents$filepath[i]
-  documents$name[i]=sub("./SMART GRANT/","",documents$name[i])
-  documents$name[i]=sub(".txt","",documents$name[i])
-  # get the text of the bill
-  raw_text = readLines(grant.list[i])
-  # collapse it together into a string
-  if(length(raw_text)>1){
-    raw_text = paste0(raw_text,collapse = " ")
+install_missing <- function(libraries) {
+  for (lib in libraries) {
+    if (!require(lib, character.only = TRUE)) {
+      install.packages(lib, dependencies = TRUE)
+      library(lib, character.only = TRUE)
+    }
   }
-  # store it in the data frame
-  documents$grant[i] = raw_text
 }
 
-# We can create a quanteda corpus object out of this character vector:
-my_corp <- corpus(x = documents$grant,
-                  docnames = documents$name,
-                  meta = list(source = "8 Smart Grants"))
-summary(my_corp)
+install_missing(libraries)
 
-#### Finding Key Words in Context based off of project demands is very easy kwic(my_tokens_defaults,pattern=phrase("whatever words or sentence you want to put in here"),window=(some number between 1-20)
-my_tokens_defaults<-tokens(my_corp)
+# Function to read documents
+read_documents <- function(directory) {
+  file_list <- list.files(directory, full.names = TRUE)
+  documents <- data.frame("filepath" = file_list,
+                          "name" = basename(file_list),
+                          "grant" = sapply(file_list, function(file) {
+                            raw_text <- tryCatch(readLines(file, warn = FALSE), error = function(e) NA)
+                            paste(raw_text, collapse = " ")
+                          }))
+  return(documents)
+}
 
-my_tokens <- tokens(my_corp,
-                    what = "word",
-                    remove_punct = TRUE, # removes punctuation
-                    remove_symbols = TRUE, # removes unicode symbols
-                    remove_numbers = TRUE, # removes numbers but not words starting with digits
-                    remove_url = TRUE, # eliminates URLs beginning with http/https
-                    remove_separators = TRUE, # removes separators as categorized in unicode
-                    include_docvars = FALSE) # whether to pass on the docvars to the tokens object
-dfm_nostopwords  <- dfm_remove(doc_term_matrix,
-                               stopwords("english"))
+# Read in documents
+documents <- read_documents("./SMART GRANT")
 
-doc_term_matrix <- dfm(my_tokens,
-                       tolower = TRUE)
+# Create corpus
+my_corp <- corpus(documents$grant, docnames = documents$name, meta = list(source = "8 Smart Grants"))
+
+# Tokenize the corpus
+my_tokens <- tokens(my_corp, what = "word", remove_punct = TRUE, remove_symbols = TRUE, 
+                    remove_numbers = TRUE, remove_url = TRUE, remove_separators = TRUE)
+
+# Create Document-Feature Matrix (DFM)
+doc_term_matrix <- dfm(my_tokens, tolower = TRUE)
+dfm_nostopwords <- dfm_remove(doc_term_matrix, stopwords("english"))
 dfm_stemmed <- dfm_wordstem(dfm_nostopwords)
 
-top_terms = data.frame("Rank" = 1:12,
-                       "Defaults" = names(topfeatures(doc_term_matrix,12)),
-                       "Processing" = names(topfeatures(dfm_stemmed,12)))
+# Find top terms
+top_terms <- data.frame("Rank" = 1:12,
+                        "Defaults" = names(topfeatures(doc_term_matrix, 12)),
+                        "Processing" = names(topfeatures(dfm_stemmed, 12)))
 
-freq_weight = textstat_frequency(dfm_nostopwords, 
-                                 n = 30)
-ggplot(data = freq_weight, 
-       aes(x = nrow(freq_weight):1, 
-           y = frequency)) +
+# Plot term frequency
+freq_weight <- textstat_frequency(dfm_nostopwords, n = 30)
+ggplot(data = freq_weight, aes(x = nrow(freq_weight):1, y = frequency)) +
   geom_point() +
   coord_flip() +
-  scale_x_continuous(breaks = nrow(freq_weight):1,
-                     labels = freq_weight$feature) +
+  scale_x_continuous(breaks = nrow(freq_weight):1, labels = freq_weight$feature) +
   labs(x = NULL, y = "Relative frequency")
 
-cleaned_text<-as.data.frame(dfm_nostopwords)
+# Convert DFM to tidy format for further analysis
+tidy_dfm <- tidy(dfm_nostopwords)
 
-word_freq<-dfm_nostopwords%>%
+# Create long format for word frequency
+word_freq <- tidy_dfm %>%
+  group_by(term) %>%
+  summarize(count = sum(count), .groups = 'drop') %>%
+  arrange(desc(count))
 
-library(wordcloud)
-library(RColorBrewer)
+# Create Grant Data
+Grant_Data <- data.frame(Name = documents$name, Text = documents$grant)
 
-
-Grant_Data<-data.frame(Name=documents$name,Text=documents$grant)
-
-
-
-
-word_freq <- cleaned_text %>%
-  count(word, sort = TRUE)
-
-
-
-####So Basically from this we kind of just know the KWIC used by each document, We're now going to make a Term Frequency Inverse Matrix, to determine if these winners in the Sensor and Smart Traffic Category are all applying under the same objective or have variance.
-install.packages("tidytext")
-library(tidytext)
-
-
-term_frequency<-tokenized_data%>%count(Name,word,sort=TRUE)
-
-
-
+# Tokenize data for term frequency calculation
+tokenized_data <- tidy_dfm %>%
+  group_by(document, term) %>%
+  summarize(n = sum(count), .groups = 'drop') %>%
+  rename(Name = document)
 
 # Calculate Inverse Document Frequency (IDF)
-total_documents <- n_distinct(term_frequency$Name)
-idf <- term_frequency %>%
-  group_by(word) %>%
-  summarize(idf = log(total_documents / n()))
-tf_idf <- term_frequency %>%
-  inner_join(idf, by = "word") %>%
+total_documents <- n_distinct(tokenized_data$Name)
+idf <- tokenized_data %>%
+  group_by(term) %>%
+  summarize(idf = log(total_documents / n()), .groups = 'drop')
+
+# Calculate TF-IDF
+tf_idf <- tokenized_data %>%
+  inner_join(idf, by = "term") %>%
   mutate(tf_idf = n * idf) %>%
   arrange(desc(tf_idf))
 
+# Create TF-IDF Matrix
 tf_idf_matrix <- tf_idf %>%
-  select(Name, word, tf_idf) %>%
-  spread(key = word, value = tf_idf, fill = 0)
+  select(Name, term, tf_idf) %>%
+  pivot_wider(names_from = term, values_from = tf_idf, values_fill = 0)
 
-# Print the TF-IDF Matrix
+# Print TF-IDF Matrix
 print(tf_idf_matrix)
 
+# Convert TF-IDF Matrix to long format for heatmap plotting
 tf_idf_long <- tf_idf_matrix %>%
-  gather(key = "word", value = "tf_idf", -Name)
+  pivot_longer(cols = -Name, names_to = "term", values_to = "tf_idf")
 
-# Plot a heatmap
-ggplot(tf_idf_long, aes(x = word, y = as.factor(Name), fill = tf_idf)) +
+# Plot heatmap
+ggplot(tf_idf_long, aes(x = term, y = as.factor(Name), fill = tf_idf)) +
   geom_tile() +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
   labs(x = "Words", y = "Documents", fill = "TF-IDF")
- 
